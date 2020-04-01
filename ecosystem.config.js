23215886d49cef7ef83346bea7bdfce2364e1f9b
davegahn <anxieter@yandex.ru>
2020-04-01 13:53:41 +0300

    Fix pm2 config

---
+++ ecosystem.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  apps : [{
+    name: 'slavsalon-server',
+    script: 'index.js',
+    autorestart: true,
+    watch: false,
+    max_memory_restart: '1G',
+    // env: {
+    //  'NODE_ENV'         : 'production',
+    //  'APP_URL'          : 'http://www.xn--80aaf8admgsd3i.xn--p1acf',
+    //  'CLIENT_URL'       : 'http://www.xn--80aaf8admgsd3i.xn--p1acf',
+    //  'APP_PORT'         : 3050,
+    //  'MONGO_URI'        : 'mongodb://127.0.0.1:27017/slavsalon',
+    //  'CYPHER_SECRET'    : 'slavs',
+    //  'SECRET_JWT_TOKEN' : 'slavsalon'
+    // }
+  }]
+};
