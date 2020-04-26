cddb18dde13da2392a5660e9e4b797e423501bc7
aleksey-pro <anxieter@yandex.ru>
2020-04-26 19:38:01 +0400

    Added mongo auth fix

--- src/bdconfig.ts
+++ src/bdconfig.ts
@@ -1,6 +1,7 @@
 import  { ConnectionOptions } from 'mongoose';
 
 export const options: ConnectionOptions = {  
+	"authSource": "admin",
 	"user": process.env.MONGO_USER,
 	"pass": process.env.MONGO_PASS,
 	"useUnifiedTopology": true,
