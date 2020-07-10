9166133917f83d8ade3ffa4e1022c5455ef6e428
aleksey-pro <anxieter@yandex.ru>
2020-07-10 18:13:31 +0400

    Fix allow f3

--- src/app.ts
+++ src/app.ts
@@ -24,6 +24,7 @@ if (process.env.NODE_ENV !== 'test') {
 
 const allowCrossDomain = function (req: Request, res: Response, next: NextFunction) {
   let listSite = ['http://www.xn--80acc6acd1bv2f.xn--p1acf','http://xn--80acc6acd1bv2f.xn--p1acf'];
+  console.log(req.headers);
   if(req.headers['origin']) {
     const origin = listSite.find(list => list === req.headers['origin'][0]) || "*";
     res.header('Access-Control-Allow-Origin', origin);
